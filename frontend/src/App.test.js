59409b756773b0451e4a7bd7d8f6041587999a4a
Siddharth-H <siddharthahatkar@gmail.com>
2019-11-25 04:30:26 -0700

    Testing for live changes

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -7,3 +7,13 @@ it('renders without crashing', () => {
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
+
+// Testing Automatic updation in test suite
+it('renders without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<App />, div);
+  ReactDOM.unmountComponentAtNode(div);
+});
+
+
+// use "docker exec -it container_id" npm run test
